f8bbe5d47cfd3d31666e0aceb06da95588348801
VickyTheCoder <vikram.the.coder@gmail.com>
2021-09-22 12:16:11 +0530

    Login page changes

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-export class Login extends React.Component
+export default class Login extends React.Component
 {
     constructor(props)
     {
@@ -10,12 +10,27 @@ export class Login extends React.Component
 
     render()
     {
-        return <div>
-            <div className="Header">Login</div>
-
-            <div className="Footer">
-                <button type="submit"></button>
-            </div>
+        return (
+        <div>
+            <table>
+                <caption>Login Page</caption>
+                <tr>
+                    <td><label>UserId</label></td>
+                    <td><input name="username"></input></td>
+                </tr>
+                <tr>
+                    <td><label>Password</label></td>
+                    <td><input name="password" type="password"></input></td>
+                </tr>
+                <tr>
+                    <td><button name="login">Login</button></td>
+                    <td><button name="Signup">Sign Up</button></td>
+                </tr>
+                <tr>
+                    <td><button name="forgot_credentials">Forgot Credentials</button></td>
+                </tr>
+            </table>
         </div>
+        );
     }
 }
